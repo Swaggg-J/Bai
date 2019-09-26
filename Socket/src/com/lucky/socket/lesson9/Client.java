adf64a444bc87cc955ae95cfaf2fab532ff897b4
Majin Boo <1309633023@qq.com>
2019-09-26 22:14:31 +0800

    多文件上传

--- Socket/src/com/lucky/socket/lesson9/Client.java
+++ Socket/src/com/lucky/socket/lesson9/Client.java
@@ -1,54 +1,73 @@
 package com.lucky.socket.lesson9;
 
+import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		//利用IP和端口号访问服务器上的网络程序
-		
-
-
+		traversalFiles("file");
 	}
-	
+
 	public static Socket getSocket() throws UnknownHostException, IOException {
 		Socket socket = new Socket("192.168.92.147",16888);
 		return socket;
 	}
 	public static void traversalFiles(String path) {
-		File file = new File(path);
-		if(!file.exists()) {
+		File f = new File(path);
+
+		if(!f.isDirectory()) {
 			return;
 		}
-		if(file.isFile()) {
-			new Thread() {
-				public void run() {
-					DataOutputStream dos = null;
-					try {
-						dos = new DataOutputStream(getSocket().getOutputStream());
-						dos.writeUTF(file.getName());
-						dos.flush();
-					} catch (UnknownHostException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}finally {
+		File[] files = f.listFiles();
+		if(files == null) {
+			return;
+		}
+		for(File file : files) {
+			if(file.isFile()) {
+				new Thread() {
+					public void run() {
+						DataOutputStream dos = null;
+						BufferedInputStream bis = null;
 						try {
+							dos = new DataOutputStream(getSocket().getOutputStream());
+							dos.writeUTF(file.getName());
 							dos.flush();
-							dos.close();
+							bis = new BufferedInputStream(new FileInputStream(file));
+							byte[] buf = new byte[1024];
+							int length = bis.read(buf);
+							if(length != -1) {
+								dos.writeUTF(new String(buf,0,length));
+								length = bis.read(buf);
+							}
+						} catch (UnknownHostException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
 						} catch (IOException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
+						}finally {
+							try {
+								dos.flush();
+								dos.close();
+								bis.close();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 						}
-					}
-				};
-			}.start();
+					};
+				}.start();
+			}
+			if(file.isDirectory()) {
+				traversalFiles(file.getAbsolutePath());
+			}
 		}
 
 	}
 }
+
--- Socket/src/com/lucky/socket/lesson9/Server.java
+++ Socket/src/com/lucky/socket/lesson9/Server.java
@@ -1,7 +1,37 @@
 package com.lucky.socket.lesson9;
 
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		//监听端口号
+		ServerSocket serverSocket = new ServerSocket(16888);
+		//等待客户端连接
+		Socket socket = serverSocket.accept();
 		
+		boolean flag = true;
+		while(flag) {
+			DataInputStream dis = new DataInputStream(socket.getInputStream());
+			File file = new File("copy"+File.separator+dis.readUTF());
+			BufferedOutputStream bos = 
+					new BufferedOutputStream(new FileOutputStream(file));
+			byte[] buf = new byte[1024];
+			int length = dis.read(buf);
+			while(length != -1) {
+				bos.write(buf);
+				bos.flush();
+				length = dis.read(buf);
+			}
+			dis.close();
+			bos.close();
+		}
+		socket.close();
 	}
 }
